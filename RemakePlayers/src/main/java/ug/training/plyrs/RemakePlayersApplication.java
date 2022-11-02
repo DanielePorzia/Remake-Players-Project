79f56841944bde4c497251ec66802fc30e52c308
Daniele <daniele.porzia@usergroup.it>
2022-11-02 10:37:50 +0100

    First commit, no files just project's creation

---
+++ RemakePlayers/src/main/java/ug/training/plyrs/RemakePlayersApplication.java
@@ -0,0 +1,13 @@
+package ug.training.plyrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemakePlayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemakePlayersApplication.class, args);
+	}
+
+}
---
+++ RemakePlayers/src/test/java/ug/training/plyrs/RemakePlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package ug.training.plyrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemakePlayersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
